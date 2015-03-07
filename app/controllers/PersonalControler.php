ef6b007051955138d3b7bdee932614f9c2733f11
trebol256 <fjcogollor@unal.edu.co>
2015-03-07 17:48:04 -0500

    prueba de modificacion

--- app/controllers/PersonalControler.php
+++ app/controllers/PersonalControler.php
@@ -5,18 +5,15 @@ class personalcontroler extends BaseController {
     public function getRegistrar($tipo) {
         if ($tipo == "medico") {
             return View::make('registro.medico');
-        }else if ($tipo=="enfermera"){
+        } else if ($tipo == "enfermera") {
             return View::make('registro.enfermera');
-        }else if ($tipo=="paciente"){
+        } else if ($tipo == "paciente") {
             return View::make('registro.paciente');
+        } else {
+            echo "error";
         }
-    else{
-        echo "error";
     }
-        
-        
-        
-        }
+    
 
 }
 
